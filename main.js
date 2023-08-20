7d4d3bc96cc4c1c4ce5dbe2dff8cbc68aff77b8b
Shashi Singh <singh.shashishekhar@gmail.com>
2023-08-21 01:45:19 +0530

    Code which shows the issue, where node_module is not copied while packaging

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const electron = require('electron');
 const app = electron.app;
 const electronUnhandled = require('electron-unhandled');
 
+const { hasScreenCapturePermission } = require('mac-screen-capture-permissions');
+
 electronUnhandled({
   showDialog: false,
   logger: error => {
@@ -13,5 +15,7 @@ electronUnhandled({
 
 app.on('ready', () => {
     console.log('Hello from Electron');
+  const hasPermission = hasScreenCapturePermission();
+  console.log("hasPermission", hasPermission);
     });
 
